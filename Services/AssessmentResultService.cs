d4da6e947b464c1743b0383841073b01042d2da8
whitetoblack <mert-cengiz@hotmail.com>
2024-05-30 21:52:59 +0300

    a

---
+++ Services/AssessmentResultService.cs
@@ -0,0 +1,7 @@
+﻿using breath_app_core.Model.DbContext;
+using BreathApp.Models;
+
+namespace breath_app_core.Services {
+    public class AssessmentResultService(BreathContext context) : GenericEntityService<AssessmentResult>(context) {
+    }
+}
